6e2e41e4b5f87722421188588ef35d68d810f27d
Francisco <jhersonhuacho@gmail.com>
2021-03-27 07:41:44 -0500

    docs agregado

---
+++ docs/precache-manifest.60f58087f76127112fd0e67c4c4e1441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076bbddc88d5056f86d01eac18541db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f56d4a4050daa970a62a",
+    "url": "/static/css/main.871b42ad.chunk.css"
+  },
+  {
+    "revision": "e19d0735e7a30d1109a6",
+    "url": "/static/js/2.6dc554c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6dc554c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56d4a4050daa970a62a",
+    "url": "/static/js/main.3dbd3690.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
